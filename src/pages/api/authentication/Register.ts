87032704288a840042e4444adc67d1eea654c586
xaanyaax <aanyasingh282004@gmail.com>
2025-07-06 19:44:37 +0530

    register User code

---
+++ src/pages/api/authentication/Register.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest , NextApiResponse } from "next";
+import { connectToDatabase } from "@/database/connect";
+import User from "@/models/userModel";
+import bcrypt from "bcrypt";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    //the method must ne POST!
+    if(req.method !== "POST") return res.status(405).end();
+
+    const {username, email, password} = req.body;
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({email: email});
+
+    if(existingUser){
+        return res.status(400).json({error: "User already exists"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({username , email , password: hashedPassword})
+
+    return res.status(201).json({message: "User created successfully"});
+}
